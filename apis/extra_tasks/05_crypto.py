447b5e5ada0e75cb28f9d7f3dba004adb2319648
Martin Breuss <martin-martin@users.noreply.github.com>
2023-03-09 20:12:52 +0100

    Update API
    Nomics API is now paid: https://forum.codingnomads.co/t/extra-tasks-api-05-crypto/1786

--- apis/extra_tasks/05_crypto.py
+++ apis/extra_tasks/05_crypto.py
@@ -1,16 +1,16 @@
 '''
-http://docs.nomics.com/
-Using the nomics API, repeatedly fetch the price of Bitcoin for a duration of 10 minutes.
+https://coinmarketcap.com/api/pricing/
+Use the CoinMarketCap API, to repeatedly fetch the price of Bitcoin for a duration of 10 minutes.
 Store each value in a dictionary that uses the time of query as a key.
 
 After the script stopped running, determine programmatically at what query time the price
 was the highest, and when the lowest.
 
 HINTS:
-- request an API key first and remember to include it in your queries
-- the /prices endpoint of the nomics API can be used for achieving this task
-- remember to use packages from the standard library, e.g. for time keeping and dates
+- You can pick a different API from this list: https://github.com/public-apis/public-apis#cryptocurrency
+- You may have to request an API key first. If you need one, remember to include it in your queries.
+- You can use packages from the standard library, e.g. for time keeping and dates
 
-BONUS: Explore the logging package for easier tracking
+BONUS: Explore the `logging` package for easier tracking
 
 '''
